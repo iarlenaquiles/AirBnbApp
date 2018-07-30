f78db79e592f7f31c6df0b382d599660a013bdbe
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-07-30 18:32:28 -0300

    Export dos styles

--- src/pages/signIn/index.js
+++ src/pages/signIn/index.js
@@ -6,94 +6,100 @@ import { StackActions, NavigationActions } from 'react-navigation';
 
 import api from '../../services/api';
 
-import { Container, Logo, Input, ErrorMessage, Button, ButtonText, SignUpLink, SignUpLinkText } from './styles';
+import {
+  Container,
+  Logo,
+  Input,
+  ErrorMessage,
+  Button,
+  ButtonText,
+  SignUpLink,
+  SignUpLinkText,
+} from './styles';
 
 export default class SignIn extends Component {
+  static navigationOptions = {
+    header: null,
+  };
 
-    static navigationOptions = {
-        header: null,
-    };
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+      dispatch: PropTypes.func,
+    }).isRequired,
+  };
 
-    static propTypes = {
-        navigation: PropTypes.shape({
-          navigate: PropTypes.func,
-          dispatch: PropTypes.func,
-        }).isRequired,
-      };
+  state = {
+    email: '',
+    password: '',
+    error: '',
+  };
 
-    state = {
-        email: '',
-        password: '',
-        error: '',
-    };
+  handleEmailChange = (email) => {
+    this.setState({ email });
+  };
 
-    handleEmailChange = (email) => {
-        this.setState({ email });
-    };
+  handlePasswordChange = (password) => {
+    this.setState({ password });
+  };
 
-    handlePasswordChange = (password) => {
-        this.setState({ password });
-    };
+  handleCreateAccountPress = () => {
+    this.props.navigation.navigate('SignUp');
+  };
 
-    handleCreateAccountPress = () => {
-        this.props.navigation.navigate('SignUp');
-    };
+  handleSignInPress = async () => {
+    if (this.state.email.length === 0 || this.state.password.length === 0) {
+      this.setState({ error: 'Preencha usuário e senha para continuar!' }, () => false);
+    } else {
+      try {
+        const response = await api.post('/sessions', {
+          email: this.state.email,
+          password: this.state.password,
+        });
 
-    handleSignInPress = async () => {
-        if (this.state.email.length === 0 || this.state.password.length === 0) {
-            this.setState({ error: 'Preencha o email e a senha para continuar!'}, () => false);
-        } else {
-            try {
-                const response = await api.post('/sessions', {
-                    email: this.state.email,
-                    password: this.state.password
-                });
+        await AsyncStorage.setItem('@AirBnbApp:token', response.data.token);
 
-                const resetAction = StackActions.reset({
-                    index: 0,
-                    actions: [
-                        NavigationActions.navigate({ routeName: 'Main', params: { token: response.data.token }}),
-                    ],
-                });
-
-                this.props.navigation.dispach(resetAction);
-            } catch(_err){
-                this.setState({ error: 'Houve um problema com o seu login, verifique as credenciais!'});
-            }
-        }
-    };
-
-    render() {
-        return(
-            <Container>
-                <StatusBar hidden/>
-                <Logo source={require('../../images/airbnb_logo.png')} resizeMode="contain" />
-                <Input 
-                    placeholder="Email"
-                    value={this.state.email}
-                    onChangeText={this.handleEmailChange}
-                    autoCapitalize="none"
-                    autoCorrect={false}
-                />
-
-                <Input 
-                    placeholder="Senha"
-                    value={this.state.password}
-                    onChangeText={this.handlePasswordChange}
-                    autoCapitalize="none"
-                    autoCorrect={false}
-                    secureTextEntry
-                />
-
-                {this.state.error.length !== 0 && <ErrorMessage>{this.state.error}</ErrorMessage>}
-                <Button onPress={this.handleSignInPress}>
-                    <ButtonText>Entrar</ButtonText>
-                </Button>
-
-                <SignUpLink onPress={this.handleCreateAccountPress}>
-                    <SignUpLinkText>Criar conta grátis</SignUpLinkText>
-                </SignUpLink>
-            </Container>
-        );
+        const resetAction = StackActions.reset({
+          index: 0,
+          actions: [
+            NavigationActions.navigate({ routeName: 'Main' }),
+          ],
+        });
+        this.props.navigation.dispatch(resetAction);
+      } catch (_err) {
+        this.setState({ error: 'Houve um problema com o login, verifique suas credenciais!' });
+      }
     }
+  };
+
+  render() {
+    return (
+      <Container>
+        <StatusBar hidden />
+        <Logo source={require('../../images/airbnb_logo.png')} resizeMode="contain" />
+        <Input
+          placeholder="Endereço de e-mail"
+          value={this.state.email}
+          onChangeText={this.handleEmailChange}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <Input
+          placeholder="Senha"
+          value={this.state.password}
+          onChangeText={this.handlePasswordChange}
+          autoCapitalize="none"
+          autoCorrect={false}
+          secureTextEntry
+        />
+        {this.state.error.length !== 0 && <ErrorMessage>{this.state.error}</ErrorMessage>}
+        <Button onPress={this.handleSignInPress}>
+          <ButtonText>Entrar</ButtonText>
+        </Button>
+        <SignUpLink onPress={this.handleCreateAccountPress}>
+          <SignUpLinkText>Criar conta grátis</SignUpLinkText>
+        </SignUpLink>
+      </Container>
+    );
+  }
 }
